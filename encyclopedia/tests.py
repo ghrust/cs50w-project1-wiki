870eb0cf27488be8d55f9a9c54ec30a0376e78db
ghrust <garaev.rustam.h@gmail.com>
2020-07-30 21:05:59 +0400

    test status code for entry pages

--- encyclopedia/tests.py
+++ encyclopedia/tests.py
@@ -1,7 +1,11 @@
 import os
+import markdown2
 
-from django.test import Client, TestCase
+from django.test import TestCase
+from django.template.defaultfilters import slugify
+
 from . import util
+
 
 # Create your tests here.
 class EncyclopediaTestCase(TestCase):
@@ -15,8 +19,22 @@ class EncyclopediaTestCase(TestCase):
 
     def test_index(self):
         """Test index page."""
+
         resp = self.client.get('/')
 
         self.assertEqual(resp.status_code, 200)
         for entry in util.list_entries():
             self.assertIn(entry, str(resp.content))
+
+    def test_entry_page(self):
+        """Test entry page."""
+
+        for entry in util.list_entries():
+            slug_entry = slugify(entry)
+            resp = self.client.get(f'/wiki/{slug_entry}')
+
+            # with open(f'./entries/{entry}.md') as ef:
+            #     entry_file_content = ef.readlines()
+
+            self.assertEqual(resp.status_code, 200)
+            # self.assertEqual(entry_file_content, resp.context['entry_content'])
