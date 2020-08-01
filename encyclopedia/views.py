f5f51c2e397a5b7dbee5cd84f47c3f7c449f0c40
ghrust <garaev.rustam.h@gmail.com>
2020-08-01 17:14:03 +0400

    title from entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -20,12 +20,13 @@ def entry_page(request, entry_name):
     """Render entry page."""
 
     with open(f'./entries/{entry_name}.md') as ef:
-        entry_file_content = ef.readlines()
+        ef_content = ef.readlines()
 
         # convert markdown to html
-        ef_content_html = markdown2.markdown(''.join(entry_file_content))
+        ef_content_html = markdown2.markdown(''.join(ef_content))
 
     context = {
+        'entry_title': entry_name,
         'entry_content': ef_content_html
     }
     return render(request, "encyclopedia/entry.html", context)
